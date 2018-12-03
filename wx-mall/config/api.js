8eca17c30f9218d3c933a2574daa2fee354e7528
zengcongcong <Freedo>
2018-12-03 15:11:59 +0800

    提交

--- wx-mall/config/api.js
+++ wx-mall/config/api.js
@@ -1,7 +1,7 @@
 const root = '/platform/api/';
 
 // const API_BASE_URL = 'http://fly2you.cn' + root;
-const API_BASE_URL = 'http://localhost:8080' + root;
+const API_BASE_URL = 'https://pokemon-rocket.top/' + root;
 module.exports = {
     IndexUrlNewGoods: API_BASE_URL + 'index/newGoods', //新品首发
     IndexUrlHotGoods: API_BASE_URL + 'index/hotGoods', //热卖商品
